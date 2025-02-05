d9c1f55419256b6ec197582680c118ceb7de1279
Amanda <rodriguesamanda49@gmail.com>
2025-02-05 19:20:55 -0300

    loading dynamic icon add e ajustes de largura para input login

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,20 @@
-import { Component } from 'react';
+import { Component } from "react";
+import "../style/Loading.css";
 
 class Loading extends Component {
   render() {
-    return (<div>Carregando...</div>);
+    return (
+      <div class="middle">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="bar bar4"></div>
+        <div class="bar bar5"></div>
+        <div class="bar bar6"></div>
+        <div class="bar bar7"></div>
+        <div class="bar bar8"></div>
+      </div>
+    );
   }
 }
 
