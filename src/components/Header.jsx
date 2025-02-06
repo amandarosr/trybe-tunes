f70d91cdf6b316210a8e09619a43105734ce32cd
Amanda <rodriguesamanda49@gmail.com>
2025-02-05 21:34:43 -0300

    header estilizada com logo e nav links em row

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { getUser } from '../services/userAPI';
-import Loading from './Loading';
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import { getUser } from "../services/userAPI";
+import Loading from "./Loading";
+import vinyl from "../extra/vinyl-record.png";
+import "../style/Header.css";
 
 class Header extends Component {
   state = {
     loading: false,
-    userName: '',
+    userName: "",
   };
 
   async componentDidMount() {
@@ -14,21 +16,40 @@ class Header extends Component {
       loading: true,
     });
     const user = await getUser();
-    this.setState({
-      userName: user.name,
-    }, () => this.setState({
-      loading: false,
-    }));
+    this.setState(
+      {
+        userName: user.name,
+      },
+      () =>
+        this.setState({
+          loading: false,
+        })
+    );
   }
 
   render() {
     const { loading, userName } = this.state;
     return (
       <header data-testid="header-component">
-        { loading ? <Loading /> : <h3 data-testid="header-user-name">{ userName }</h3> }
-        <Link to="/search" data-testid="link-to-search"> Pesquisar </Link>
-        <Link to="/favorites" data-testid="link-to-favorites"> Favoritos </Link>
-        <Link to="/profile" data-testid="link-to-profile"> Perfil </Link>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="username-title">
+            <img alt="logo" src={vinyl} className="header-logo" />
+            <h3 className="header-username">{userName + "'s tunes"}</h3>
+          </div>
+        )}
+        <nav>
+          <Link to="/search" className="link link-to-search">
+            Pesquisar
+          </Link>
+          <Link to="/favorites" className=" link link-to-favorites">
+            Favoritos
+          </Link>
+          <Link to="/profile" className="link link-to-profile">
+            Perfil
+          </Link>
+        </nav>
       </header>
     );
   }
