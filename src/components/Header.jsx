c2c9b636d77506c5118e234173268f5899fc6811
Amanda <rodriguesamanda49@gmail.com>
2025-02-06 17:08:21 -0300

    background da homepage mais clara, com header sem background. Links search e favorites dinamicos em configuração de switch dependendo do pathname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,47 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-import { getUser } from "../services/userAPI";
-import Loading from "./Loading";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import PropTypes from "prop-types";
 import vinyl from "../extra/vinyl-record.png";
+import profile from "../extra/profile-user.png";
 import "../style/Header.css";
 
 class Header extends Component {
-  state = {
-    loading: false,
-    userName: "",
-  };
-
-  async componentDidMount() {
-    this.setState({
-      loading: true,
-    });
-    const user = await getUser();
-    this.setState(
-      {
-        userName: user.name,
-      },
-      () =>
-        this.setState({
-          loading: false,
-        })
-    );
-  }
-
   render() {
-    const { loading, userName } = this.state;
+    const { path } = this.props.match;
     return (
       <header data-testid="header-component">
-        {loading ? (
-          <Loading />
-        ) : (
-          <div className="username-title">
-            <img alt="logo" src={vinyl} className="header-logo" />
-            <h3 className="header-username">{userName + "'s tunes"}</h3>
-          </div>
-        )}
+        <div className="username-title">
+          <img alt="logo" src={vinyl} className="header-logo" />
+          <h3 className="header-username">noTunes</h3>
+        </div>
         <nav>
-          <Link to="/search" className="link link-to-search">
+          <Link
+            to="/search"
+            className={path === "/search" ? "link current-page" : "link"}
+          >
             Pesquisar
           </Link>
-          <Link to="/favorites" className=" link link-to-favorites">
+          <Link
+            to="/favorites"
+            className={path === "/favorites" ? "link current-page" : "link"}
+          >
             Favoritos
           </Link>
-          <Link to="/profile" className="link link-to-profile">
-            Perfil
-          </Link>
         </nav>
+        <Link to="/profile" className="link profile-div">
+          <img alt="profile" src={profile} className="profile-icon" />
+          Perfil
+        </Link>
       </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string,
+  }),
+};
+
+export default withRouter(Header);
